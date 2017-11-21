0cae47639bb9167a9c270eab32132c115f8d303d
Cerberus\Belal Ehmed <belalehmed.propanol@gmail.com>
2017-11-22 02:36:54 +0500

    Delegate implementation

---
+++ Assets/Delegates n Events/Scripts/Delegates/DelegateNeo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DelegateNeo 
+{
+    //Private
+    private string name;
+
+    //Constructor
+    public DelegateNeo(string _name)
+    {
+        name = _name;
+    }
+
+    //Delegate declaration
+    public delegate void SimpleDelegate();
+    public SimpleDelegate OnSimpleDelegate;
+}
---
+++ Assets/Delegates n Events/Scripts/Delegates/DelegateNeoImp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateNeoImp : MonoBehaviour {
+
+    DelegateNeo delObj;
+
+    void Awake()
+    {
+        delObj = new DelegateNeo("delegate object");
+
+        //Delegate instantiation.
+        delObj.OnSimpleDelegate = new DelegateNeo.SimpleDelegate(DelFunc);
+    }
+    
+    void Start() 
+    {
+        //Delegate invoked
+        delObj.OnSimpleDelegate();
+    }
+
+    //Callback method
+    void DelFunc() 
+    {
+        Debug.Log("Callback invoked");
+    }
+}
